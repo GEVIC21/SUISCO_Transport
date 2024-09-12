590bb38a3b16528980183364f5c0ce47b7d83f98
fadile <fadbrown980@gmail.com>
2024-09-12 16:11:12 +0000

    admin consignation

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -93,6 +93,26 @@ License: For each use you must have a valid license purchased only from above li
 									</a>
 								</div>
                                 <div  class="menu-item menu-accordion">
+									<a class="menu-link @if(Route::currentRouteName() == 'admin.Consignations') active @endif" href="{{route('admin.Consignations')}}">
+										<span class="menu-icon">
+											<!--begin::Svg Icon | path: icons/duotune/general/gen025.svg-->
+                                            <span class="svg-icon">
+                                                <!--begin::Svg Icon | path: assets/media/icons/duotune/communication/com008.svg-->
+                                                <span class="svg-icon ">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                                    <path opacity="0.3" d="M4.425 20.525C2.525 18.625 2.525 15.525 4.425 13.525L14.825 3.125C16.325 1.625 18.825 1.625 20.425 3.125C20.825 3.525 20.825 4.12502 20.425 4.52502C20.025 4.92502 19.425 4.92502 19.025 4.52502C18.225 3.72502 17.025 3.72502 16.225 4.52502L5.82499 14.925C4.62499 16.125 4.62499 17.925 5.82499 19.125C7.02499 20.325 8.82501 20.325 10.025 19.125L18.425 10.725C18.825 10.325 19.425 10.325 19.825 10.725C20.225 11.125 20.225 11.725 19.825 12.125L11.425 20.525C9.525 22.425 6.425 22.425 4.425 20.525Z" fill="black"/>
+                                                    <path d="M9.32499 15.625C8.12499 14.425 8.12499 12.625 9.32499 11.425L14.225 6.52498C14.625 6.12498 15.225 6.12498 15.625 6.52498C16.025 6.92498 16.025 7.525 15.625 7.925L10.725 12.8249C10.325 13.2249 10.325 13.8249 10.725 14.2249C11.125 14.6249 11.725 14.6249 12.125 14.2249L19.125 7.22493C19.525 6.82493 19.725 6.425 19.725 5.925C19.725 5.325 19.525 4.825 19.125 4.425C18.725 4.025 18.725 3.42498 19.125 3.02498C19.525 2.62498 20.125 2.62498 20.525 3.02498C21.325 3.82498 21.725 4.825 21.725 5.925C21.725 6.925 21.325 7.82498 20.525 8.52498L13.525 15.525C12.325 16.725 10.525 16.725 9.32499 15.625Z" fill="black"/>
+                                                    </svg>
+                                                </span>
+                                                <!--end::Svg Icon-->
+                                            </span>
+                                            <!--end::Svg Icon-->
+											<!--end::Svg Icon-->
+										</span>
+										<span class="menu-title">Consignations</span>
+									</a>
+								</div>
+                                <div  class="menu-item menu-accordion">
 									<a class="menu-link @if(Route::currentRouteName() == 'admin.ecoles') active @endif" href="{{route('admin.ecoles')}}">
 										<span class="menu-icon">
 											<!--begin::Svg Icon | path: icons/duotune/general/gen025.svg-->
@@ -207,7 +227,13 @@ License: For each use you must have a valid license purchased only from above li
 										<div class="menu menu-lg-rounded menu-column menu-lg-row menu-state-bg menu-title-gray-700 menu-state-title-primary menu-state-icon-primary menu-state-bullet-primary menu-arrow-gray-400 fw-bold my-5 my-lg-0 align-items-stretch" id="#kt_header_menu" data-kt-menu="true">
 											<div  data-kt-menu-placement="bottom-start" class="menu-item  menu-lg-down-accordion me-lg-1">
 												<a  class="menu-link py-3 @if(Route::currentRouteName() == 'admin.reservations') active @endif" href="{{route('admin.reservations')}}">
-													<span class="menu-title">Reservations</span>
+													<span class="menu-title">Réservations</span>
+													<span class="menu-arrow d-lg-none"></span>
+												</a>
+											</div>
+                                            <div  data-kt-menu-placement="bottom-start" class="menu-item  menu-lg-down-accordion me-lg-1">
+												<a  class="menu-link py-3 @if(Route::currentRouteName() == 'admin.Consignations') active @endif" href="{{route('admin.Consignations')}}">
+													<span class="menu-title">Consignations</span>
 													<span class="menu-arrow d-lg-none"></span>
 												</a>
 											</div>
@@ -270,7 +296,7 @@ License: For each use you must have a valid license purchased only from above li
 											<!--end::Menu separator-->
 											<!--begin::Menu item-->
 											<div class="menu-item px-5">
-												<a href="../../demo1/dist/account/overview.html" class="menu-link px-5">Mon profil</a>
+												<a href="{{route('admin.update.utilisateur',auth()->user()->id)}}" class="menu-link px-5">Mon profil</a>
 											</div>
 											<!--end::Menu item-->
 											<!--begin::Menu separator-->
@@ -336,22 +362,11 @@ License: For each use you must have a valid license purchased only from above li
 						<div class="container-fluid d-flex flex-column flex-md-row align-items-center justify-content-between">
 							<!--begin::Copyright-->
 							<div class="text-dark order-2 order-md-1">
-								<span class="text-muted fw-bold me-1">2022©</span>
-								<a href="https://keenthemes.com" target="_blank" class="text-gray-800 text-hover-primary">Keenthemes</a>
+								<span class="text-muted fw-bold me-1">2024©</span>
+								<a href="https://keenthemes.com" target="_blank" class="text-gray-800 text-hover-primary">Itplex | Suisco</a>
 							</div>
 							<!--end::Copyright-->
 							<!--begin::Menu-->
-							<ul class="menu menu-gray-600 menu-hover-primary fw-bold order-1">
-								<li class="menu-item">
-									<a href="https://keenthemes.com" target="_blank" class="menu-link px-2">About</a>
-								</li>
-								<li class="menu-item">
-									<a href="https://devs.keenthemes.com" target="_blank" class="menu-link px-2">Support</a>
-								</li>
-								<li class="menu-item">
-									<a href="https://1.envato.market/EA4JP" target="_blank" class="menu-link px-2">Purchase</a>
-								</li>
-							</ul>
 							<!--end::Menu-->
 						</div>
 						<!--end::Container-->
@@ -4004,6 +4019,9 @@ License: For each use you must have a valid license purchased only from above li
 		<script src="assets/js/custom/apps/chat/chat.js"></script>
 		<script src="assets/js/custom/utilities/modals/upgrade-plan.js"></script>
 		<script src="assets/js/custom/utilities/modals/users-search.js"></script>
+
+        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
 
         @yield('scripts')
 		<!--end::Page Custom Javascript-->
